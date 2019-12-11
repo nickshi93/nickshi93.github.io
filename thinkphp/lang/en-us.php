e6e3561f3367fb28ca15e26ae699e4efebbe5a9e
nickshi93 <54701647+nickshi93@users.noreply.github.com>
2019-12-11 17:17:33 +0800

    Update en-us.php

--- thinkphp/lang/en-us.php
+++ thinkphp/lang/en-us.php
@@ -2,9 +2,21 @@
 
 	return [
 	
+		//头部
 		'zn'=>'CN',
 		
 		'en'=>'EN',
+		
+		'home'=>'Home',
+		
+		'login' =>'Login',
+		
+		'logout' =>'Logout',
+		
+		'member'=>'Member Center',
+		
+			
+		
 		
 		'goods'=>'goods',
 		
